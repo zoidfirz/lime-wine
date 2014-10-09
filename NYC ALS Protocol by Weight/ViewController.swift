ecc869aa3064ec9b5d1292625b2d297eda6e013d
Jason Sutherland <zoidfirz@gmail.com>
2014-10-09 15:11:35 -0400

    buttons in place, with layout constraints

--- NYC ALS Protocol by Weight/ViewController.swift
+++ NYC ALS Protocol by Weight/ViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
+    @IBOutlet weak var enterWeightTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
 
 }
 
