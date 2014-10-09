eff1ca10822fa907ad814ade032d0f63d49eab91
Jason Sutherland <zoidfirz@gmail.com>
2014-10-09 14:12:32 -0400

    Initial Commit

---
+++ NYC ALS Protocol by Weight/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NYC ALS Protocol by Weight
+//
+//  Created by Jason Sutherland on 10/9/14.
+//  Copyright (c) 2014 Jason Sutherland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
